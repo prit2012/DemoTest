b522fa3e764b56ad36b25139dc898a4e669bbaa1
Piyush <piyushdmistry2012@gmail.com>
2018-03-03 22:59:17 +0530

    sys.out line modified

--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -3,7 +3,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("This is demo for Git");
 		
-		System.out.println("this is first branch change");
+		System.out.println("while marging this brance to master branch here a conflict will occour");
 	}
 
 }
