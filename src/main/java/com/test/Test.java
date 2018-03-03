fdc388e42b0417eb0616ea339bbdc2520c2c4bb0
Piyush <piyushdmistry2012@gmail.com>
2018-03-03 22:31:46 +0530

    added a sys.out in main method

--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -2,6 +2,8 @@ package com.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("This is demo for Git");
+		
+		System.out.println("this is first branch change");
 	}
 
 }
